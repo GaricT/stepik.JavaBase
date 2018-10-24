fcc8c4a1a11ff23de017a23493ffac8f995c98c8
garic-r@yandex.ru <n88jT6xsEheTGTPBLH9WO6T2d8>
2018-10-24 18:43:08 +0300

    Глава 2. Примитивные типы Тестовые задание в разделе 2.1 Возвращает букву, стоящую в таблице UNICODE после символа "\" (обратный слэш) на расстоянии a.

---
+++ src/chapter2/Exercise_22_1.java
@@ -0,0 +1,17 @@
+package chapter2;
+
+public class Exercise_22_1 {
+    public static void main(String[] args) {
+        char c = '\\';
+        int a = 32;
+//        Your code complexity score is 2.24 (best for this step is 2.24).
+        c = (char)('\\' + a);
+        c = (char)(92 + a);
+//        Your code complexity score is 2.45 (best for this step is 2.24).
+        c = (char)(a+=92);
+//        Your code complexity score is 3.0 (best for this step is 2.24).
+        c = (c+=a);
+        System.out.println(Integer.toHexString(c) + ":" + c);
+        System.out.println(c + '');
+    }
+}
