8b924ab4cf76ea2e96750bcaa7d008064eb7d33d
garic-r@yandex.ru <n88jT6xsEheTGTPBLH9WO6T2d8>
2018-10-25 16:49:28 +0300

    Глава 3. Массивы и строки Преобразование строк к значению: A12

---
+++ src/chapter2/Exercise_23_1.java
@@ -0,0 +1,14 @@
+package chapter2;
+
+/*
+ ** Преобразование строк к значению: A12
+ */
+
+public class Exercise_23_1 {
+    public static void main(String[] args) {
+        System.out.println('A' + '1' + "2");
+        System.out.println("A" + 12);
+        System.out.println("A" + ('\t' + '\u0003'));
+        System.out.println('A' + "12");
+    }
+}
