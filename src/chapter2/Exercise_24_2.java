2e5d792282d2fc33a3a021e89b1bdaa3533123e0
garic-r@yandex.ru <n88jT6xsEheTGTPBLH9WO6T2d8>
2018-10-25 19:48:15 +0300

    Глава 4. Условные операторы и циклы Сортировка слияением (улучшенный но не упоротый вариант)

--- src/chapter2/Exercise_24_2.java
+++ src/chapter2/Exercise_24_2.java
@@ -5,16 +5,16 @@ import java.util.Arrays;
 
 public class Exercise_24_2 {
     public static void main(String[] args) {
-//        int[] a1 = new int[29];
+//        int[] a1 = {};
 //        int[] a2 = {};
-//        int[] a1 = new int[29];
-//        int[] a2 = new int[9];
-//        for (int i = 0; i< a1.length; i++){
-//            a1[i] = i;
-//        }
-//        for (int i = 0; i< a2.length; i++){
-//            a2[i] = i;
-//        }
+        int[] a1 = new int[29];
+        int[] a2 = new int[9];
+        for (int i = 0; i< a1.length; i++){
+            a1[i] = i;
+        }
+        for (int i = 0; i< a2.length; i++){
+            a2[i] = i;
+        }
 
 
 //        int[] a1 = {0, 2, 2};
@@ -23,26 +23,24 @@ public class Exercise_24_2 {
 
     }
     public static int[] mergeArrays(int[] a1, int[] a2) {
+        if ((a1.length == 0) || (a2.length == 0)){
+            return (a1.length == 0) ? a2 : a1;
+        }
+
         int b1 = 0;
         int b2 = 0;
         int max_len = a1.length + a2.length;
         int[] res = new int[max_len];
 
-        if (a1.length == 0 || a2.length == 0){
-            return (a1.length == 0) ? a2 : a1;
-        }
         for (int i = 0; i< max_len; i++){
 
-            if (b1 >= a1.length){
-                res[i] = a2[b2++];
+            if (b1 == a1.length || b2 == a2.length){
+                res[i] = (b1 == a1.length) ? a2[b2++] : a1[b1++];
+                continue;
             }
-            else if (b2 >= a2.length){
-                res[i] = a1[b1++];
-            }
-            else{
-                res[i] = (a1[b1] < a2[b2])? a1[b1++] : a2[b2++];
-            }
+            res[i] = (a1[b1] < a2[b2])? a1[b1++] : a2[b2++];
         }
+
         return res; // your implementation here
     }
 }
