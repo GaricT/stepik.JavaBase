eb1d23ecff3874babc050c63ac447dbfb6bafb54
garic-r@yandex.ru <n88jT6xsEheTGTPBLH9WO6T2d8>
2018-10-31 17:15:06 +0300

    Глава 3.3 Объявление класса Выберите верные утверждения про модификатор static.

---
+++ src/chapter3/Exercise_33_1.java
@@ -0,0 +1,15 @@
+package chapter3;
+
+public class Exercise_33_1 {
+
+    static class TstStatic  {
+        public int id;
+        public TstStatic(int arg){
+            id = arg;
+        }
+    }
+    public static void main(String[] args) {
+//        TstStatic.i
+    }
+}
+
