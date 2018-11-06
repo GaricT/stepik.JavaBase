7f7e48c3323ce029509a2085af43ea59cde90518
garic-r@yandex.ru <n88jT6xsEheTGTPBLH9WO6T2d8>
2018-11-06 19:30:32 +0300

    Глава 4.2 Знакомство с исключениями Метод, позволяющий другим методам узнать, откуда их вызвали.

---
+++ src/chapter4/Exercise_41_2.java
@@ -0,0 +1,25 @@
+package chapter4;
+
+public class Exercise_41_2 {
+    public static void main(String[] args) {
+        System.out.println(getCallerClassAndMethodName());
+        anotherMethod();
+    }
+
+    private static void anotherMethod() {
+        System.out.println(getCallerClassAndMethodName());
+        includeMethod();
+    }
+
+    private static void includeMethod() {K
+        System.out.println(getCallerClassAndMethodName());
+    }
+
+    public static String getCallerClassAndMethodName() {
+        StackTraceElement[] stack = new Throwable().getStackTrace();
+        if (stack.length <= 2)
+            return null;
+        else
+            return stack[2].getClassName() + "#" + stack[2].getMethodName();
+    }
+}
